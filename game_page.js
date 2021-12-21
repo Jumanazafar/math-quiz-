ce0a5c20467f1cc1b3f29cc07b6c9a3f395e37ba
Jumanazafar <88046631+Jumanazafar@users.noreply.github.com>
2021-12-21 21:01:20 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -17,5 +17,47 @@ function send(){
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     row=question_word+input_box+check_button;
     document.getElementById("output").innerHTML=row;
-    document.getElementById("word").value="";
+    
+}
+question_turn="player1";
+answer_turn="player2";
+function check(){
+    get_answer=document.getElementById("input_check_box").value;
+    answer=get_answer.toLowerCase();
+    if(answer==actualans)
+    {
+        if(answer_turn=="player1")
+        {
+            player1_score=player1_score+1;
+            document.getElementById("player1_score").innerHTML=player1_score;
+        }
+        else
+        {
+            player2_score=player2_score+1;
+            document.getElementById("player2_score").innerHTML=player2_score;   
+        }
+    }
+    if(answer_turn=="player1")
+    {
+        answer_turn="player2";
+        document.getElementById("player_ans").innerHTML="Answer turn- "+player2_name;
+
+    }
+    else
+    {
+        answer_turn="player1";
+        document.getElementById("player_ans").innerHTML="Answer turn- "+player1_name;  
+    }
+    if(question_turn=="player1")
+    {
+        question_turn="player2";
+        document.getElementById("player_ques").innerHTML="Question turn- "+player2_name;
+
+    }
+    else
+    {
+        question_turn="player1";
+        document.getElementById("player_ques").innerHTML="Question turn- "+player1_name;  
+    }
+document.getElementById("output").innerHTML="";
 }
